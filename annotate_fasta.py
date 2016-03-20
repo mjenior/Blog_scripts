d02503b08ffc75df0537019c88acb625bad979e8
Matt Jenior <mljenior@umich.edu>
2016-03-20 15:20:14 -0400

    added functionaty to annotate_fasta.py

--- annotate_fasta.py
+++ annotate_fasta.py
@@ -21,6 +21,9 @@ with open(sys.argv[2],'r') as blast_output:
 key_errors = 0
 start = 0
 hypothetical = 0
+unknown = 0
+uncharacterized = 0
+host = 0
 
 # Parse the fasta file and print translated names and unchanged sequence data
 for line in fasta_file:
@@ -34,6 +37,9 @@ for line in fasta_file:
 		try:
 			blast_hit = blast_dictionary[entry]
 			if 'hypothetical' in blast_hit: hypothetical += 1
+			if 'unknown' in blast_hit: unknown += 1
+			if 'uncharacterized' in blast_hit: uncharacterized += 1
+			if 'mmu' in blast_hit: host += 1
 		except KeyError:
 			key_errors += 1
 			blast_hit = entry + ':Unknown_' + str(key_errors)
@@ -48,9 +54,17 @@ for line in fasta_file:
 fasta_file.close()
 output_file.close()
 
-annotated = start - key_errors
-print(str(start)) + ' total sequences.'
-print(str(annotated) + ' sequences successfully annotated.')
-print(str(hypothetical) + ' sequences annotated as hypothetical.')
+print(str(start) + ' total sequences.')
 print(str(key_errors) + ' sequences could not be annotated.')
-
+print('\n')
+annotated = start - key_errors
+print(str(annotated) + ' sequences recieved some annotation.')
+genomes = int(annotated / 3000)
+print('\tor ~' + str(genomes) + ' genome equivalents.')
+print(str(hypothetical) + ' sequences annotated as hypothetical.')
+print(str(unknown) + ' sequences annotated as unknown function.')
+print(str(uncharacterized) + ' sequences annotated as uncharacterized.')
+print(str(host) + ' sequences annotated as host.')
+print('\n')
+final = annotated - hypothetical - unknown - uncharacterized - host
+print(str(final) + ' sequences have an informative annotation.')
